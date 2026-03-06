85d356efd7b2e4e3a7e5aaeeb91b50528ceb9984
Riri <htn290806@gmail.com>
2026-03-06 16:00:17 +0700

    comit cho du

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    java
+    application
+    idea
+    id ("com.diffplug.spotless") version "8.2.1"
+    id("com.gradleup.shadow") version "9.3.1"
+}
+
+apply(from = "repositories.gradle")
+apply(from = "dependencies.gradle")
+
+group = property("group") as String
+version = property("version") as String
+
+application {
+    mainClass.set("riri.BookStoreApp")
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes(
+                "Main-Class" to application.mainClass.get()
+            )
+        }
+    }
+
+    shadowJar {
+        archiveClassifier.set("all")
+        manifest {
+            attributes(
+                "Main-Class" to application.mainClass.get()
+            )
+        }
+    }
+}
+
+spotless {
+    java {
+        target("src/**/*.java")
+    }
+
+    format("misc") {
+        target(
+            "*.gradle.kts",
+            "*.md",
+            ".gitignore"
+        )
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "Software"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
